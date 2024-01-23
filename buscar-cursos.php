114bfdd03b6c57f5a4b73e46b31f2392b7bdad49
Paulo Martins <paulo_martins@sapo.pt>
2024-01-23 12:55:42 +0000

    Solve certifacate error

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -7,7 +7,8 @@ use Alura\BuscadorDeCursos\Buscador;
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
-$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
+// Adicionado porque estava a dar erro de certificado
+$client = new Client(['base_uri' => 'http://www.alura.com.br/', 'verify' => false]);
 $crawler = new Crawler();
 
 $buscardor = new Buscador($client, $crawler);
